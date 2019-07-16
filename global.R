5348b644fd0fa291d9092bee13b03c6063e348e0
colinchristie <colinchristie54@gmail.com>
2019-07-16 15:01:03 -0400

    Add files via upload

--- global.R
+++ global.R
@@ -1,3 +1,4 @@
+.libPaths("/home/maya/R/x86_64-pc-linux-gnu-library/3.4/") #mapview dependencies, use only for online version
 library(shiny)
 library(lubridate)
 library(dplyr)
@@ -12,49 +13,111 @@ library(leaflet)
 library(raster)
 library(shinyjs)
 library(mapview)
+library(data.table)
+library(feather)
 
-app.data <- read.csv("./databases/all_data.csv", header = TRUE)
+#Memory limit increase (online only)
+#library(httr)
+#library(ulimit)
+#library(feather)
+#set_config( config( ssl_verifypeer = 0L ))
+#ulimit::memory_limit(4000)
 
-app.data$Timestamp <- force_tz(ymd_hms(app.data$Timestamp), tz = "America/New_York") #Reads in timestamp as integer, so converts to POSIXct
+#Use this one if online:
+#app.data <- read_feather("/srv/shiny-server/databases/sapphirine/all_data.feather")
 
-app.data <- mutate(app.data, Day = date(Timestamp), Time = strftime(Timestamp, format = "%H:%M"), Count = 1) 
-#Creates two new columns for later subsetting of data by date and by time, as well as one column for measurement density count
+#Use this one if locally making adjustments:
+#Local
+#app.data <- read_feather("./databases/all_data.feather")
 
-mins <- unique(strftime(force_tz(as_datetime(ymd_hm("1970-01-01 00:00") : ymd_hm("1970-01-01 23:59")), tz = "America/New_York"), format = "%H:%M"))
-mins.5 <- unique(strftime(floor_date(force_tz(as_datetime(ymd_hm("1970-01-01 00:00") : ymd_hm("1970-01-01 23:59")), tz = "America/New_York"), unit = "5 min"), format = "%H:%M"))
-mins.5 <- c(mins.5, "23:59")
-#Creates 2 lists of times from 0:00 to 23:59, one by the minute and one by 5 minute intervals, given as strings
-#5 minute intervals will be used on slider
-#1 minute interval list will be indexed by a "grep" function using the 5 minute interval choices
+#Online
+app.data <- read_feather("/srv/shiny-server/databases/cdatabases/sapphirine/all_data.feather")
 
-assign("df.list.2", as.list(read.csv("./databases/df_list.csv", stringsAsFactors = FALSE)[1,]), envir = .GlobalEnv)
+hours <- c("00:00",
+           "01:00",
+           "02:00",
+           "03:00",
+           "04:00",
+           "05:00",
+           "06:00",
+           "07:00",
+           "08:00",
+           "09:00",
+           "10:00",
+           "11:00",
+           "12:00",
+           "13:00",
+           "14:00",
+           "15:00",
+           "16:00",
+           "17:00",
+           "18:00",
+           "19:00",
+           "20:00",
+           "21:00",
+           "22:00",
+           "23:00",
+           "23:59"
+            )
 
-titles.list <- list( "Avg. Temp. (\u00B0C)", "Avg. Humidity (%)", "Avg. PM1 Conc. (\u03BCg/m\u00B3)", "Avg. PM2.5 Conc. (\u03BCg/m\u00B3)", "Avg. PM10 Conc. (\u03BCg/m\u00B3)")
+mins <- unique(strftime(force_tz(as_datetime(as_datetime(hm("0:00")): as_datetime(hm("23:59"))), tz = "America/New_York"), format = "%H:%M"))
+#Creates character list that will be indexed for temporal subsetting
 
-abbrs.list <- list("Temp.", "Humidity", "PM1", "PM2.5", "PM10")
+sensor.measures <- c("Temperature", "Humidity", "PM1", "PM2.5", "PM10")
+other.measures <- c("Crime", "Poverty", "Traffic")
+all.measures <- c(sensor.measures, other.measures)
 
-titles.df <- data.frame(cbind(df.list.2, titles.list, abbrs.list))
+titles.list <- c("Avg. Temp. (\u00B0C)", "Avg. Humidity (%)", "Avg. PM1 Conc. (\u03BCg/m\u00B3)", 
+                     "Avg. PM2.5 Conc. (\u03BCg/m\u00B3)", "Avg. PM10 Conc. (\u03BCg/m\u00B3)", 
+                     "# of Reported Crimes", "Avg. ADI", "Avg. AADT")
+
+suffix.list <- c(".t", ".h", ".pm1", ".pm2.5", ".pm10", ".c", ".pov", ".tr")
+
+titles.df <- data.frame(cbind(all.measures, titles.list, suffix.list))
 
 f.titles <- function(y){
   
-  if(y %in% df.list.2){  
-    index <- which(titles.df[,1] == y)
-    return(titles.df[index, 2])
-  }
-  
-  else if(length(grep(".mdb", y)) > 0){
-    index <- grep(gsub(".mdb", "", y), titles.df[,1])
-    return(paste("log # of", titles.df[index, 3], "Data Points"))
-  }
-  
+  index <- which(titles.df[,1] == y)
+  return(titles.df[index, 2])
+ 
 } #End f.titles
 
-f.units <- function(z){
-  index <- grep(paste(z), titles.df[,1])
-  return(paste(titles.df[index, 3]))
+f.titles.d <- function(w){paste("log # of", w, "data points")}
+
+f.suffix <- function(z){
+  
+  index <- which(titles.df[,1] == z)
+  return(titles.df[index, 3])
+  
 }
 
-our.sensors <- read.csv("./databases/LIMEA_AIRBEAM_SUMMARY.csv", header = TRUE, stringsAsFactors = FALSE)$AirBeamID[1:15]
+#Online:
+pov.raster <- raster("/srv/shiny-server/databases/cdatabases/sapphirine/poverty.grd")
+
+#Local:
+#pov.raster <- raster('./databases/poverty.grd')
+
+#Online:
+our.sensors <- fread("/srv/shiny-server/databases/cdatabases/sapphirine/LIMEA_AIRBEAM_SUMMARY.csv", header = TRUE, stringsAsFactors = FALSE)$AirBeamID[1:15]
+
+#Local:
+#our.sensors <- fread("./databases/LIMEA_AIRBEAM_SUMMARY.csv", header = TRUE, stringsAsFactors = FALSE)$AirBeamID[1:15]
+
+#Online:
+city.border <- read.csv("/srv/shiny-server/databases/cdatabases/sapphirine/city_border.csv", header = TRUE)[,2:3]
+
+#Local:
+#city.border <- read.csv("./databases/city_border.csv", header = TRUE)[,2:3]
+
+#Online:
+traffic.df <- readRDS("/srv/shiny-server/databases/cdatabases/sapphirine/phlroads.rds")
+
+#Local:
+#traffic.df <- readRDS("./databases/phlroads.rds")
+
+traffic.df$CUR_AADT <- as.numeric(as.character(traffic.df$CUR_AADT))
+traffic.df <- spTransform(traffic.df, CRS("+proj=longlat +datum=WGS84"))
+
 our.sensors <- paste0("AirBeam:", our.sensors)
 
 sensor.names <- names(table(app.data$Sensor.ID))
--- ui.R
+++ ui.R
@@ -1,3 +1,5 @@
+
+.libPaths("/home/maya/R/x86_64-pc-linux-gnu-library/3.4/") #mapview dependencies, use only for online version
 library(shiny)
 library(lubridate)
 library(dplyr)
@@ -18,55 +20,30 @@ source("global.R")
 #Define user interface
 ui <- fluidPage(
   
-  titlePanel("Sensor-based Analysis of Polution in the Philadelphia Region (SAPPhiRe)"),
+  titlePanel("Sensor-based Analysis of Polution in the Philadelphia Region with Information on Neighborhoods and the Environment (SAPPhiRINE)"),
   
   fluidRow(
     
     column(12,
-           tags$p("SAPPhiRe is an interactive geospatial-analysis tool that allows users to visualize pollution data throughout Philadelphia as recorded by portable sensors.
+           tags$p("SAPPhiRINE is an interactive geospatial-analysis tool that allows users to visualize pollution data throughout Philadelphia as recorded by portable sensors.
                   We include data downloaded from Aircasting's CrowdMap, data downloaded from the PurpleAir website, and data collected independently with our own sensors."),
            tags$p("Adjust the parameters below to your desired values, then click \"Go\" to display the corresponding map.
                   On the map below, click on a bin to view the data corresponding to the region it encompasses as well as to your selected parameters.
                   Click on the crosshair icon to recenter the map.")
            )
-           ),
+   ),
   
   wellPanel(
     
     fluidRow(
       
-      column(3,
-             pickerInput("vars",
-                         label = "Plot",
-                         choices = list(
-                           "Temperature" = "Temperature",
-                           "Humidity" = "Humidity",
-                           "PM1" = "PM1",
-                           "PM2.5 - continuous" = "PM2.5",
-                           "PM2.5 - discrete" = "PM2.5.d",
-                           "PM10" = "PM10"
-                         ),
-                         multiple = FALSE,
-                         selected = "PM2.5"
-             ),
-             
-             radioButtons("d.type",
-                          label = "Display type",
-                          choices = list(
-                            "Measurement value" = "mv",
-                            "Measurement density" = "md"
-                          )
-             )
-             
-      ), #End column 1
-      
-      column(3,
+      column(2,
              #Dropdown list for selecting data by sensor
              pickerInput("sensors.hl",
                          label = "Himes Lab sensors to include",
                          choices = subset(sensor.names, sensor.names %in% our.sensors),
                          multiple = TRUE,
-                         selected = sensor.names,
+                         selected = subset(sensor.names, sensor.names %in% our.sensors),
                          options = list(`actions-Box` = TRUE, `none-Selected-Text` = "None")
              ),
              
@@ -74,21 +51,22 @@ ui <- fluidPage(
                          label = "Other sensors to include",
                          choices = subset(sensor.names, !sensor.names %in% our.sensors),
                          multiple = TRUE,
-                         selected = sensor.names,
+                         selected = subset(sensor.names, !sensor.names %in% our.sensors),
                          options = list(`actions-Box` = TRUE, `none-Selected-Text` = "None")
              )
              
-      ),
+      ), #End column 1
       
-      column(3,
+      column(2,
              
              #Dual textbox for entering in a date range
-             dateRangeInput("dates", label = "Date range", start = "2014-01-01", end = Sys.Date(), startview = "decade"),
+             dateRangeInput("dates", label = "Date range", start = "2015-06-01", end = "2019-05-31", startview = "decade",
+                            min = "2015-06-01", max = "2019-05-31"),
              
              #Slider bar for selecting a time-of-day-range
              sliderTextInput("times",
                              label = "Time of day",
-                             choices = mins.5,
+                             choices = hours,
                              selected = c("00:00", "23:59"),
                              force_edges = TRUE,
                              width = "50%"
@@ -96,31 +74,57 @@ ui <- fluidPage(
              
       ), #End column 2
       
-      column(3,
+      column(2,
+             
+             #Slider for selecting raster boundaries
+             sliderInput("lat.range",
+                         label = "Latitude Range",
+                         min = 38.85,
+                         max = 40.60,
+                         value = c(39.87, 40.14),
+                         step = 0.00001,
+                         sep = ""
+                         ),
+             
+             sliderInput("lon.range",
+                         label = "Longitude Range",
+                         min = -76.40,
+                         max = -74.40,
+                         value = c(-75.28, -74.97),
+                         step = 0.00001,
+                         sep = ""
+                         )
+      ), #End column 3
+      
+      column(2,
              
              #Slider for selecting number of bins
-             sliderInput("bin.no",
-                         label = "Resolution (# of bins per row/column)",
-                         min = 5,
-                         max = 125,
-                         value = 75
-             ),
+             sliderInput("row.no",
+                         label = "Rows",
+                         min = 2,
+                         max = 200,
+                         value = 100
+                        ),
              
-             fluidRow(
-               column(3,
-                      actionButton("go", "Go")
-               ),
-               column(6,
-                      actionButton("ss", "Export map image")
-               )
-             )
+             sliderInput("col.no",
+                         label = "Columns",
+                         min = 2,
+                         max = 200,
+                         value = 100
+                         )
              
-      ) #End column 3
-    ) #End WellPanel
+      ), #End column 4
+      
+      column(2,
+             actionButton("go", "Go"),
+             downloadButton("ss", "Export map image")
+      ) #End column 5
+      
+    ) #FluidRow1
     
     #Checklist for selection of measurement type
     
-  ), #End row 1
+  ), #End WellPanel
   
   fluidRow(
     leafletOutput("int.map", height = 700)
